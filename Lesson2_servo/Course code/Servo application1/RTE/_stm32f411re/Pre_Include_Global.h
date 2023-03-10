9a4815c1d56413e513f76a12de41e725e3391ed7
Akinboyede Joseph <josephakinboyede16@gmail.com>
2023-03-10 16:41:08 +0100

    My STM32 Class.

---
+++ Lesson2_servo/Course code/Servo application1/RTE/_stm32f411re/Pre_Include_Global.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'servo_main_app' 
+ * Target:  'stm32f411re' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil::Device:STM32Cube HAL:Common:1.8.1 */
+#define USE_HAL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ Lesson2_servo/Course code/Servo application1/main.cpp
@@ -0,0 +1,38 @@
+#include "servo.h"
+
+int main(void)
+{
+	HAL_Init();  // Initialize the HAL API 
+	__HAL_RCC_GPIOB_CLK_ENABLE();  // Enable the HAL_GPIO clock (This is are macros)
+	__HAL_RCC_TIM4_CLK_ENABLE();   // Enable the HAL_TIMER clock
+
+	// This create pinStruct type for servoPin
+	pinStruct_t servoPin;
+	servoPin.port = GPIOB;
+	servoPin.selectedPin = GPIO_PIN_8;
+	
+	static Servo servo (servoPin, TIM4, GPIO_AF2_TIM4, TIM_CHANNEL_3);     // this is a servo object
+		
+	while(1)
+	{
+		servo.TurnShaft(SERVO_MIDPOSITION);
+		HAL_Delay(1000);
+		servo.TurnShaft(SERVO_LEFTPOSITION);
+		HAL_Delay(1000);
+		servo.TurnShaft(SERVO_MIDPOSITION);
+		HAL_Delay(1000);
+		servo.TurnShaft(SERVO_RIGHTPOSITION);
+		HAL_Delay(1000);
+				
+	}
+	
+	
+	
+}
+
+//The Method for HAL delay function this can be find in the startup file.s
+extern "C" void SysTick_Handler(void)
+{
+	HAL_IncTick();
+
+}
